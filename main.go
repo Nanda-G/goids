3f86ef6a4569394d8d019736699deb04164ed696
Ayaan Zaidi <biblioklept@icloud.com>
2020-04-09 04:48:06 +0530

    Changed progress bar definition to set

--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 
 	"github.com/schollz/progressbar"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/winservices"
 )
 
 func main() {
 	v, _ := mem.VirtualMemory()
+	services, _ := winservices.ListServices()
+	bar := progressbar.New(100)
 
 	for {
-		bar := progressbar.New(100)
-		bar.Add(int(v.UsedPercent))
+		fmt.Printf("Services: %v", services)
 		fmt.Printf(" Total: %v, Free:%v, UsedPercent:%v%%\n", v.Total, v.Free, int64(v.UsedPercent))
+		bar.Set(int(v.UsedPercent))
+
 		time.Sleep(2 * time.Second)
 	}
 }
