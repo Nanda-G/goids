311faad69cc60edd6ed329b9aa87ad09c9d9a23e
Ayaan Zaidi <biblioklept@icloud.com>
2020-04-11 15:12:44 +0530

    Fixed bad reference to getinfo dependancy

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"goids/getinfo"
+	"github.com/obviyus/goids/getinfo"
 
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/host"
@@ -14,14 +14,11 @@ import (
 
 func main() {
 	cpu, _ := cpu.Info()
-	// fmt.Printf("Raw CPU data: %v", c)
 	fmt.Printf("Cores: %v \tCPU Model: %v \n", cpu[0].Cores, cpu[0].ModelName)
 
 	getinfo.DiskInfo()
 
 	//TODO: List all processes
-	// p, _ := process.Processes()
-	// fmt.Printf("\nList of Processes Running: \n")
 	infoStat, _ := host.Info()
 	fmt.Printf("Total processes: %d\t", infoStat.Procs)
 	miscStat, _ := load.Misc()
